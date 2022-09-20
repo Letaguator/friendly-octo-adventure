02d1edd8211adb7e1e49ef6ac1dd8eccd2e0a63d
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-09-20 15:08:55 -0400

    Update code and add perf measuring

--- ProjectOne/main.erl
+++ ProjectOne/main.erl
@@ -1,10 +1,8 @@
-%%% @author ruiyang
+%%% @author ruiyang & mathias.brekkan
 %%% run start_master/2 on the server
 %%% run start_slave/1 on the working machine
 
-
-
-
+% N is the number of leading zeros for a potential coin
 
 -module(main).
 
@@ -12,68 +10,80 @@
 -import(rnd, [rnd_chars_numbers/1]).
 -import(binary, [decode_unsigned/1]).
 -import(crypto, [hash/1]).
+-import(timer, [apply_after/4]).
 
--export([start_master/2, start_slave/1, mine/3, slave/1, master/2]).
-
-
-
+-export([start/1, start_master/2, mine/3, slave/1, master/2, master/3, start_slaves/2, start_perf_analyzer/1]).
 
 
+master(WorkerNodeCount, AmountOfCoins, N) ->
+    start_slaves(WorkerNodeCount, node()),
+    master(AmountOfCoins, N).
 
-
-
-
-master(Work_load, N) ->
+master(AmountOfCoins, N) ->
     receive
         {slave, Slave_ID} ->                                                
-            Slave_ID ! {Work_load, N},
-            master(Work_load, N);
+            Slave_ID ! {AmountOfCoins, N},
+            master(AmountOfCoins, N);
         {found, Key, Hash} ->
             io:format("~p:", [Key]),
             io:format("~64.16.0b~n", [Hash]),
-            master(Work_load, N);
+            master(AmountOfCoins, N);
         finished ->
             io:format("job done~n", []),
-            master(Work_load, N)
+            master(AmountOfCoins, N)
     end.
 
 
+<<<<<<< HEAD
 
 
 mine(0, N, Master_Node) ->
    
  {master, Master_Node} ! finished;
 mine(Work_load, N, Master_Node) ->
+=======
+mine(0, _, Master_Node) ->
+    {master, Master_Node} ! finished;
+
+mine(AmountOfCoins, N, Master_Node) ->
+>>>>>>> 9a44ad0 (Update code and add perf measuring)
     Key = concat("liruiyang;", rnd:rnd_chars_numbers(10)),
     Hash = binary:decode_unsigned(crypto:hash(sha256, Key)),
     case Hash < math:pow(16, 64 - N) of
         true ->
             {master, Master_Node} ! {found, Key, Hash},
-            mine(Work_load - 1, N, Master_Node);
+            mine(AmountOfCoins - 1, N, Master_Node);
         false ->
-            mine(Work_load, N, Master_Node)
+            mine(AmountOfCoins, N, Master_Node)
     end.
-
-
 
 
 slave(Master_Node) ->
+    io:fwrite("awef"),
     {master, Master_Node} ! {slave, self()},
     
     receive
-        {Work_load, N} ->
-            mine(Work_load, N, Master_Node)
+        {AmountOfCoins, N} ->
+            mine(AmountOfCoins, N, Master_Node)
     end.
 
 
+start_slaves(0, _) -> ok;
+start_slaves(N, Master_Node) ->
+    spawn(main, slave, [Master_Node]),
+    start_slaves(N - 1, Master_Node).
 
+start(NumberOfLeadingZeroesInHash) ->
+    start_perf_analyzer(0),
+    start_master(16, NumberOfLeadingZeroesInHash).
 
+start_perf_analyzer(LastCpuTime) ->
+    {CpuTime, _} = statistics(runtime),
+    io:fwrite("CPU Time - Time Passed Ratio: "),
+    io:write((CpuTime - LastCpuTime) / 5000),
+    io:fwrite("\n"),
+    apply_after(5000, main, start_perf_analyzer, [CpuTime]).
 
-start_master(Work_load, N) ->
-    register(master, spawn(main, master, [Work_load, N])).
-
-start_slave(Master_Node) ->
-    register(slave, spawn(main, slave, [Master_Node])).
-
-
-
+start_master(AmountOfCoins, N) ->
+    AmountOfWorkerNodes = 5,
+    register(master, spawn(main, master, [AmountOfWorkerNodes, AmountOfCoins, N])).
