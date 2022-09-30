103566096b6c407b0a211a051c16244f0ae9245a
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-09-30 11:34:42 -0400

    Add basics for ProjectTwo

---
+++ ProjectTwo/main.erl
@@ -0,0 +1,30 @@
+% @author Mathias.Brekkan
+
+
+-module(main)
+
+-export([start/3])
+
+start(NumberOfNodes, fullNetwork, gossip) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, grid, gossip) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, line, gossip) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, imperfectGrid, gossip) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, fullNetwork, pushSum) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, grid, pushSum) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, line, pushSum) ->
+    io:fwrite("Test").
+
+start(NumberOfNodes, imperfectGrid, pushSum) ->
+    io:fwrite("Test").
