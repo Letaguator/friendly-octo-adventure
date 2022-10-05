6e597aec0e283285782c14b189e3c84b2a702c0e
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-05 14:53:57 -0400

    Refactor code, add support for line layout

--- ProjectTwo/main.erl
+++ ProjectTwo/main.erl
@@ -1,34 +1,33 @@
 % @author Mathias.Brekkan
 
-
 -module(main).
+-export([start/3, boss/4, nodeInit/1]).
 
--export([start/3, nodeInit/1, boss/2]).
+start(NumberOfNodes, GridType, AlgorithmType) ->
+    Pid = spawn(main, boss, [NumberOfNodes, GridType, AlgorithmType, []]),
+    register(master, Pid),
+    createNodes(NumberOfNodes, node()).
 
 createNodes(0, _) -> ok;
 createNodes(NumberOfNodesLeft, Master_Node) ->
     spawn(main, nodeInit, [Master_Node]),
     createNodes(NumberOfNodesLeft - 1, Master_Node).
 
-start(NumberOfNodes, GridType, AlgorithmType) ->
-    register(master, spawn(main, boss, [NumberOfNodes, []])),
-    createNodes(NumberOfNodes, node()).
-
 getRandomNumber(Min, Max) ->
     crypto:rand_uniform(Min, Max + 1).
 
-sendAllRegAcc(CurrentIndex, Nodes, []) -> ok;
-sendAllRegAcc(CurrentIndex, Nodes, [Node | Tail]) ->
-    Node ! {allRegAcc, CurrentIndex, Nodes},
-    sendAllRegAcc(CurrentIndex + 1, Nodes, Tail).
+sendAllRegAcc(CurrentIndex, Nodes, GridType, AlgorithmType, []) -> ok;
+sendAllRegAcc(CurrentIndex, Nodes, GridType, AlgorithmType, [Node | Tail]) ->
+    Node ! {allRegAcc, CurrentIndex, GridType, AlgorithmType, Nodes},
+    sendAllRegAcc(CurrentIndex + 1, Nodes, GridType, AlgorithmType, Tail).
 
-boss(NumberOfNodes, Nodes) ->
+boss(NumberOfNodes, GridType, AlgorithmType, Nodes) ->
     case NumberOfNodes == length(Nodes) of
         true ->
-            sendAllRegAcc(1, Nodes, Nodes),
             io:fwrite("\n"),
             io:write(lists:nth(getRandomNumber(1, length(Nodes)), Nodes)),
             io:fwrite("\n"),
+            sendAllRegAcc(1, Nodes, GridType, AlgorithmType, Nodes),
             lists:nth(getRandomNumber(1, length(Nodes)), Nodes) ! {gossip, "Advanced message"};
         false ->
             ok
@@ -36,29 +35,47 @@ boss(NumberOfNodes, Nodes) ->
     receive
         {reg, Slave_ID} -> % Register node                            
             io:fwrite("Node registered\n"),
-            boss(NumberOfNodes, [Slave_ID | Nodes])
+            boss(NumberOfNodes, GridType, AlgorithmType, [Slave_ID | Nodes])
     end.
 
 nodeInit(Master_Node) ->
     {master, Master_Node} ! {reg, self()},
     receive
-        {allRegAcc, Index, Nodes} ->
-            gossip(Master_Node, Index, Nodes, "", 0)
+        {allRegAcc, Index, GridType, AlgorithmType, Nodes} ->
+            case AlgorithmType of
+                "Gossip" ->
+                    gossip(GridType, Master_Node, Index, Nodes, "", 0);
+                "PushSum" ->
+                    pushSum(GridType)
+            end
     end.
 
-gossip(Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount) ->
+pushSum(GridType) ->
+    io:fwrite("pushSum").
+
+gossip(GridType, Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount) ->
     TerminationCount = 10,
     if
         RecievedMessageCount < TerminationCount ->
             receive
                 {gossip, Message} ->
                     io:format("Message recieved:~p\n", [Message]),
-                    lists:nth(getRandomNeighbour("FullNetwork", Index, Nodes), Nodes) ! {gossip, Message},
-                    gossip(Master_Node, Index, Nodes, Message, RecievedMessageCount + 1)
+                    lists:nth(getRandomNeighbour(GridType, Index, Nodes), Nodes) ! {gossip, Message},
+                    gossip(GridType, Master_Node, Index, Nodes, Message, RecievedMessageCount + 1)
             end;
         true ->
-            lists:nth(getRandomNeighbour("FullNetwork", Index, Nodes), Nodes) ! {gossip, ActualMessage},
-            gossip(Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount + 1)
+            lists:nth(getRandomNeighbour(GridType, Index, Nodes), Nodes) ! {gossip, ActualMessage},
+            gossip(GridType, Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount + 1)
+    end.
+
+adjustToLinearBounds(TargetIndex, Count) ->
+    if
+        TargetIndex > Count ->
+            1;
+        TargetIndex < 1 ->
+            Count;
+        true ->
+            TargetIndex
     end.
 
 getRandomNeighbour(GridType, Index, Nodes) ->
@@ -68,12 +85,11 @@ getRandomNeighbour(GridType, Index, Nodes) ->
             % Currently node can select itself TODO!
             getRandomNumber(1, length(Nodes));
         "Line" ->
-            % Currently node can go outside range
             case getRandomNumber(0, 1) of
                 0 ->
-                    Target = Index + 1;
+                    adjustToLinearBounds(Index + 1, NodeCount);
                 1 ->
-                    Target = Index - 1
+                    adjustToLinearBounds(Index - 1, NodeCount)
             end;
         "2dGrid" -> 
             ok;
@@ -81,5 +97,3 @@ getRandomNeighbour(GridType, Index, Nodes) ->
             ok
     end.
     
-pushSum() ->
-    io:fwrite("pushSum").
