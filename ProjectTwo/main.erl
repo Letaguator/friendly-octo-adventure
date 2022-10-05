1dad24d4ff53b3b794a0a3173d2d5f4c339bce57
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-04 22:57:02 -0400

    Update gossip, README.MD and start supporting grids

--- ProjectTwo/main.erl
+++ ProjectTwo/main.erl
@@ -67,20 +67,43 @@ nodeInit(Master_Node) ->
     {master, Master_Node} ! {reg, self()},
     receive
         {allRegAcc, Index, Nodes} ->
-            gossip(Master_Node, Index, Nodes)
+            gossip(Master_Node, Index, Nodes, "", 0)
     end.
 
-gossip(Master_Node, Index, Nodes) ->
-    receive
-        {gossip, Message} ->                   
-            io:fwrite("Message recieved:"),
-            io:fwrite(Message),
-            io:fwrite("\n"),
-            gossip(Master_Node, Index, Nodes)   
+gossip(Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount) ->
+    TerminationCount = 10,
+    if
+        RecievedMessageCount < TerminationCount ->
+            receive
+                {gossip, Message} ->
+                    io:format("Message recieved:~p\n", [Message]),
+                    lists:nth(getRandomNeighbour("FullNetwork", Index, Nodes), Nodes) ! {gossip, Message},
+                    gossip(Master_Node, Index, Nodes, Message, RecievedMessageCount + 1)
+            end;
+        true ->
+            lists:nth(getRandomNeighbour("FullNetwork", Index, Nodes), Nodes) ! {gossip, ActualMessage},
+            gossip(Master_Node, Index, Nodes, ActualMessage, RecievedMessageCount + 1)
     end.
 
-% getRandomNeighbour(GridType, Index, Nodes) ->
-%     1;
+getRandomNeighbour(GridType, Index, Nodes) ->
+    NodeCount = length(Nodes),
+    case GridType of
+        "FullNetwork" ->
+            % Currently node can select itself TODO!
+            getRandomNumber(1, length(Nodes));
+        "Line" ->
+            % Currently node can go outside range
+            case getRandomNumber(0, 1) of
+                0 ->
+                    Target = Index + 1;
+                1 ->
+                    Target = Index - 1
+            end;
+        "2dGrid" -> 
+            ok;
+        "Imperfect2dGrid" ->
+            ok
+    end.
     
 pushSum() ->
     io:fwrite("pushSum").
