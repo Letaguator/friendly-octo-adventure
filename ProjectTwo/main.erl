5eafa89b38b0b035a8b05e85b5e7a411a57d704f
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-05 14:07:51 -0400

    Remove old code

--- ProjectTwo/main.erl
+++ ProjectTwo/main.erl
@@ -5,30 +5,6 @@
 
 -export([start/3, nodeInit/1, boss/2]).
 
-% start(NumberOfNodes, fullNetwork, gossip) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, grid, gossip) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, line, gossip) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, imperfectGrid, gossip) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, fullNetwork, pushSum) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, grid, pushSum) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, line, pushSum) ->
-%     io:fwrite("Test").
-
-% start(NumberOfNodes, imperfectGrid, pushSum) ->
-%     io:fwrite("Test").
-
 createNodes(0, _) -> ok;
 createNodes(NumberOfNodesLeft, Master_Node) ->
     spawn(main, nodeInit, [Master_Node]),
