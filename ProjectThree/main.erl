b29ed7ca8cc655bdb7225d048a20bd684744cd57
Ruiyang Li <ruiyangl@outlook.com>
2022-10-22 02:44:46 -0400

    create lookup
    I did not test the code. There must be lot of bugs. We can test the code after we finish join() functionality.

--- ProjectThree/main.erl
+++ ProjectThree/main.erl
@@ -2,7 +2,7 @@
 
 -module(main).
 -export([start/2, master/4, sendAllRegAcc/6]).
--import(methods, [getRandomNumber/2, getRandomString/1, getHash/1, adjustToLinearBounds/2]).
+-import(methods, [getRandomNumber/2, getRandomString/1, getHash/1, adjustToLinearBounds/2, operate/5, getM/0]).
 
 start(NumberOfNodes, NumberOfRequests) ->
     M = math:ceil(log2(NumberOfNodes * NumberOfRequests * 2)),
@@ -15,7 +15,13 @@ createNodes(NumberOfNodesLeft, MasterNode) ->
     spawn(main, nodeInit, [MasterNode]),
     createNodes(NumberOfNodesLeft - 1, MasterNode).
 
-nodeInit(MasterNode) ->
+
+
+
+%%% node API
+%%% after creation, node process send out its PID and identifier {reg, PID, identifier}
+%%% waits for the master to send back the finger list, the number of requests the PID and the identifier of the predecessor and the Successor
+node(MasterNode) ->
     %%% in the paper the ip address is the key so I changed the name
     NodeKey = getRandomString(8),
     NodeId = getHash(NodeKey),
@@ -28,18 +34,31 @@ nodeInit(MasterNode) ->
 
     receive
         {allRegAcc, FingerList, NumberOfRequests, Predecessor, Successor} ->
-            operate(FingerList, NumberOfRequests, Predecessor, Successor)
+            operate(NodeId, FingerList, NumberOfRequests, Predecessor, Successor)
     end,
     
 
 
+findSuccessor(NodeId, FingerList, Predecessor, Successor) ->
+    if 
+        ((KeyId > NodeId) and (KeyId < Successor#node.id) ->
+            Successor;
+        true ->
+            closestPrecedingNode(KeyId).
 
-operate(FingerList, NumberOfRequestLeft) ->
+closestPrecedingNode(KeyId, FingerList, ) ->
+
+
+
+
+
+operate(NodeId, FingerList, NumberOfRequestLeft, Predecessor, Successor) ->
     receive
-        {lookUp, KeyId, WhoAsked} -> 
+        {findSuccessor, KeyId, WhoAsked} -> 
 
-            if (KeyId) 
-        
+            Res = findSuccessor(NodeId, FingerList, Predecessor, Successor),
+            WhoAsked ! {KeyId, Successor, NumHops}
+
         {found, KeyId, FoundWhere, NumHops} ->
             io:format("Node: ~p~n", [self()]),
             io:format("Key: ~p~n", [KeyId]),
@@ -109,40 +128,3 @@ nodeLoop(Hid, MasterNode, NumberOfRequests, Predecessor, FingerTable) ->
 %     NextNodeInList = list:nth(NodesSortedByHid, adjustToLinearBounds(NextNodeInListIndex, NumberOfNodes)),
 %     buildFingerList(CurrentIndex, NumberOfNodes, NodesSortedByHid, FingerTableSize, RemainingEntries - 1, [NextNodeInList | FingerList]).
 
-sendAllRegAcc(_, _, _, _, []) -> ok;
-sendAllRegAcc(NumberOfNodes, CurrentIndex, NumberOfRequests, Nodes, [Entry | Tail]) ->
-    if
-        CurrentIndex == 1 ->
-            Entry ! {create, NumberOfRequests};
-        true ->
-            Entry ! {join, list:nth(Nodes, 1)}
-    end,
-    sendAllRegAcc(FingerTableSize, NumberOfNodes, CurrentIndex + 1, NodesSortedByHid, NumberOfRequests, Tail).
-
-masterWaitForFinish(NumberOfNodesLeft) ->
-    if
-        NumberOfNodesLeft == 0 ->
-            io:format("Finished running program...");
-        true ->
-            receive
-                {finito} -> % Node completed all required requests
-                    masterWaitForFinish(NumberOfNodesLeft - 1)
-            end
-    end.
-
-master(NumberOfNodes, NumberOfRequests, Nodes, NodesMap) ->
-    case NumberOfNodes == length(Nodes) of
-        true ->
-            % FingerTableSize = round(math:log2(NumberOfNodes)),
-            % NodesSortedByHid = lists:keysort(1, maps:to_list(NodesMap)),
-            sendAllRegAcc(NumberOfNodes, 1, NumberOfRequests, Nodes, Nodes),
-            bossWaitForFinish(NumberOfNodes);
-            % Boss print average number of hops
-        false ->
-            ok
-    end,
-    receive
-        {reg, Slave_ID, Hid} -> % Register node
-            UpdatedNodesMap = maps:put(Hid, Slave_ID, NodesMap),
-            master(NumberOfNodes, NumberOfRequests, Nodes, UpdatedNodesMap)
-    end.
