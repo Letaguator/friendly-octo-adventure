25423697536c38eaa863bef6408622f7c82a315b
Ruiyang Li <ruiyangl@outlook.com>
2022-10-22 03:29:18 -0400

    records moved to .hrl file
    records are now properly stored in .hrl file and called with -include() in the main.

---
+++ ProjectThree/records.hrl
@@ -0,0 +1,7 @@
+%%% Since in lerlang records only exist in the compiler
+%%% We need to put macros and records into .hrl files and -include() in the main
+
+
+%%% I suggest we should use records to keep track of keys and nodes
+-record(node, {id, pid}). 
+-record(key, {id, key}).
