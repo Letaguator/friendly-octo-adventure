d73be85174b74afcf88a17fbf40090e2df10b3c7
Ruiyang Li <ruiyangl@outlook.com>
2022-10-23 20:41:47 -0400

    Added test interval

--- ProjectThree/test.erl
+++ ProjectThree/test.erl
@@ -4,7 +4,7 @@
 -import(methods, [getRandomNumber/2, getRandomString/1, getHash/1, adjustToLinearBounds/2]).
 
 
--export([printList/1, testHash/1]).
+-export([printList/1, testHash/1, testInterval/1]).
 
 
 
@@ -12,6 +12,17 @@ printList(List) ->
      io:fwrite("~w~n",[List]).
 
 
+testInterval(1) ->
+     Time = 100,
+     Message = "aaa",
+     timer:send_interval(Time, Message),
+     {ok, data};
+
+testInterval(handle) ->
+     receive
+          {data} ->
+               ok
+     end.
 
 
 
