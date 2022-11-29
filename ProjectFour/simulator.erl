32695c361755cbdb9053314703015fab2293a038
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-11-29 16:56:12 -0500

    Add zipf distro working version

--- ProjectFour/simulator.erl
+++ ProjectFour/simulator.erl
@@ -1,12 +1,17 @@
 % @author Mathias Brekkan and Ruiyang Li
+-module(simulator).
+-export([zipf/3, zipfSumPart/4]).
 
 % Formula from http://www.math.wm.edu/~leemis/chart/UDR/PDFs/Zipf.pdf
-zipf(x, a, n) ->
-    1/(math:pow(x, a) * zipfSumPart(1, a, n, 0)).
-zipfSumPart(n, a, n, res) ->
-    res;
-zipfSumPart(i, a, n, res) ->
-    zipfSumPart(i + 1, a, n, res + math:pow(1/i, a)).
+% In our case x can be number of subscribers, n can be maximum amount of subscribers
+% a can be 1 for now
+zipf(X, A, N) ->
+    1 / (math:pow(X, A) * zipfSumPart(1, A, N, 0)).
+
+zipfSumPart(N, _, N, Res) ->
+    Res;
+zipfSumPart(I, A, N, Res) ->
+    zipfSumPart(I + 1, A, N, Res + math:pow(1 / I, A)).
 
 
 % Take in number N of users to simulate
