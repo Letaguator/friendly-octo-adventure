adb087d7bdb0a2b7e1792779c872c586179b3a96
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-11-29 16:28:45 -0500

    Add skeleton code for simulator

--- ProjectFour/simulator.erl
+++ ProjectFour/simulator.erl
@@ -1,1 +1,18 @@
 % @author Mathias Brekkan and Ruiyang Li
+
+% Take in number N of users to simulate
+% Create a list of N usernames
+% Give every user a random amount of subscribers 
+% For every user with subscriber S, make S users take their username as input for subscription
+% Give every user a online/offline behaviour, determining how often they connect/disconnect, zipf
+% Give every user a tweet frequency rate, the higher the subscriber count S, the higher the rate TFR, zipf
+% Give every user a retweet rate, the higher the subscriber count S, the higher the retweet rate RR, zipf
+% Start simulation
+
+% Measure number of tweets pr. second
+% Measure number X
+% Measure number Y
+% Measure number Z
+% Measure number ?
+% Measure number ?
+% Measure number ?
