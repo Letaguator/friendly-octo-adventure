d19050731719470768a9208bd5802dcab00e24ec
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-11-29 23:58:26 -0500

    Fix lingo

--- ProjectFour/engine.erl
+++ ProjectFour/engine.erl
@@ -58,8 +58,8 @@ engineTick(Users, ActiveUsers, UserFollowersMap, UserRecievedTweetsMap, UserSent
             Followers = maps:get(Username, UserFollowersMap, []),
             MentionedUsers = Tweet#tweet.mentions,
             FollowersOfHashTags = getAllUsersFromHashTags(HashTagSubscriptions, Tweet#tweet.hashTags, []),
-            AllFollowersListRaw = lists:append(lists:append(lists:append(Followers, MentionedUsers), FollowersOfHashTags), [Tweet#tweet.originalTweeter, Tweet#tweet.actualTweeter]),
-            AllUsersNeedingTweet = sets:to_list(sets:from_list(AllFollowersListRaw)),
+            AllUsersNeedingTweetRaw = lists:append(lists:append(lists:append(Followers, MentionedUsers), FollowersOfHashTags), [Tweet#tweet.originalTweeter, Tweet#tweet.actualTweeter]).
+            AllUsersNeedingTweet = sets:to_list(sets:from_list(AllUsersNeedingTweetRaw)),
             
             NewUserRecievedTweetsMap = updateRecievedTweetMap(Tweet, UserRecievedTweetsMap, AllUsersNeedingTweet),
             
