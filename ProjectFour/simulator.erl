5028dbc7bf9f058ffdc4ae84b54395ec74b7667a
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-11-30 22:13:59 -0500

    Update simulator.erl and README.MD

--- ProjectFour/simulator.erl
+++ ProjectFour/simulator.erl
@@ -23,6 +23,9 @@ getRandomString(Length) ->
         [], lists:seq(1, Length)
     ).
 
+getRandomNumber(Min, Max) ->
+    crypto:rand_uniform(Min, Max + 1).
+
 getUsernamesList(0, UsernamesList, _) ->
     UsernamesList;
 getUsernamesList(NumberOfUsers, UsernamesList, NumberOfTotalUsers) ->
@@ -30,12 +33,37 @@ getUsernamesList(NumberOfUsers, UsernamesList, NumberOfTotalUsers) ->
     UserPop = #userPop{username = Username, popularity = zipf(NumberOfUsers, 3, NumberOfTotalUsers)},
     getUsernamesList(NumberOfUsers - 1, [UserPop | UsernamesList], NumberOfTotalUsers).
 
+allocateSubscribers(Users, [], NumberOfUsers, SubscriberMap) ->
+    SubscriberMap;
+allocateSubscribers(Users, [CurrentUser | UserLeft], NumberOfUsers, SubscriberMap) ->
+    Username = CurrentUser#userPop.username,
+    UserPop = CurrentUser#userPop.popularity,
+    TotalSubscriberCount = round(UserPop * NumberOfUsers),
+    RandomIndex = getRandomNumber(1, NumberOfUsers),
+    findSubscribersForUser(Username, Users, RandomIndex, TotalSubscriberCount, SubscriberMap),
+    allocateSubscribers(Users, UserLeft, NumberOfUsers, SubscriberMap).
+
+findSubscribersForUser(CurrentUsername, Usernames, IndexToFetchUsersFrom, 0, SubscriberMap) ->
+    SubscriberMap;
+findSubscribersForUser(CurrentUsername, Users, IndexToFetchUsersFrom, TotalSubscriberCount, SubscriberMap) ->
+    CurrentUserWhoWillSubscribe = lists:nth(IndexToFetchUsersFrom, Users),
+    UsernameWhoWillSubscribe = CurrentUserWhoWillSubscribe#userPop.username,
+    UsersUserWillSubscribeTo = maps:get(UsernameWhoWillSubscribe, SubscriberMap, []),
+    NewSubscriberMap = maps:put(UsernameWhoWillSubscribe, UsersUserWillSubscribeTo ++ CurrentUsername, SubscriberMap),
+    NextIndex = IndexToFetchUsersFrom + 1,
+    if
+        NextIndex > length(Users) ->
+            findSubscribersForUser(CurrentUsername, Users, 1, TotalSubscriberCount - 1, SubscriberMap);
+        true ->
+            findSubscribersForUser(CurrentUsername, Users, NextIndex, TotalSubscriberCount - 1, SubscriberMap)
+    end.
 
 startSim(NumberOfUsers) ->
-    Usernames = getUsernamesList(NumberOfUsers, [], NumberOfUsers ),
+    Usernames = getUsernamesList(NumberOfUsers, [], NumberOfUsers),
+    SubscriberMap = allocateSubscribers(Usernames, Usernames, NumberOfUsers, #{}),
+    io:write(SubscriberMap),
     io:write(Usernames).
-% Create a list of N usernames
-% Give every user a random amount of subscribers 
+
 % For every user with subscriber S, make S users take their username as input for subscription
 % Give every user a online/offline behaviour, determining how often they connect/disconnect, zipf
 % Give every user a tweet frequency rate, the higher the subscriber count S, the higher the rate TFR, zipf
