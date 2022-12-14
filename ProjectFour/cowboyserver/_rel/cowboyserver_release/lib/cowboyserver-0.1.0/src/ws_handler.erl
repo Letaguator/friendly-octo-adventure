4a4d5ece36a5b42cfeb40a92a8c4afe0af9ca15a
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-12-13 22:01:05 -0500

    Add improvements

--- ProjectFour/cowboyserver/_rel/cowboyserver_release/lib/cowboyserver-0.1.0/src/ws_handler.erl
+++ ProjectFour/cowboyserver/_rel/cowboyserver_release/lib/cowboyserver-0.1.0/src/ws_handler.erl
@@ -2,6 +2,8 @@
 -behavior(cowboy_handler).
 -export([init/2, websocket_init/1, websocket_handle/2, websocket_info/2]).
 -include("records.hrl"). 
+-import(string,[concat/2, substr/3]). 
+
 
 init(Req, State) ->
 	{cowboy_websocket, Req, State}.
@@ -11,68 +13,86 @@ websocket_init(State) ->
 
 % Recieve data from client
 websocket_handle({text, Msg}, State) ->
-	io:fwrite("websocket_handle\n"),
 	ParsedData = string:lexemes(Msg, "|"),
-	io:fwrite("lexems done\n"),
 	handleRequestCaller(State, ParsedData),
-	io:fwrite("handleRequest done\n"),
     {reply, {text, Msg}, State};
 websocket_handle(_Frame, State) ->
     {ok, State}.
 
 handleRequestCaller(State, [ MessageType | MessageTokens]) ->
 	StrMessageType = binary_to_list(MessageType),
-	handleRequest(State, StrMessageType, MessageTokens).
+	StrMessageTokens = binListToStringList([], MessageTokens),
+	printList(StrMessageTokens),
+	handleRequest(State, StrMessageType, StrMessageTokens).
 
+handleRequest(_State, "Ping", _MessageTokens) ->
+	ok;
 handleRequest(_State, "register", MessageTokens) ->
-		Username = lists:nth(1, MessageTokens),
-			engine ! {register, Username};
+	Username = lists:nth(1, MessageTokens),
+	engine ! {register, Username};
 handleRequest(_State, "logIn", MessageTokens) ->
-		Username = lists:nth(1, MessageTokens),
-		engine ! {logIn, Username, self()};
+	Username = lists:nth(1, MessageTokens),
+	engine ! {logIn, Username, self()};
 handleRequest(_State, "logOut", MessageTokens) ->
-		Username = lists:nth(1, MessageTokens),
-		engine ! {logOut, Username};
+	Username = lists:nth(1, MessageTokens),
+	engine ! {logOut, Username};
 handleRequest(_State, "followUser", MessageTokens) ->
-		MyUsername = lists:nth(1, MessageTokens),
-		Username = lists:nth(2, MessageTokens),
-		engine ! {followUser, MyUsername, Username};
+	MyUsername = lists:nth(1, MessageTokens),
+	Username = lists:nth(2, MessageTokens),
+	engine ! {followUser, MyUsername, Username};
 handleRequest(_State, "followHashtag", MessageTokens) ->
-		MyUsername = lists:nth(1, MessageTokens),
-		HashTag = lists:nth(2, MessageTokens),
-		engine ! {followHashTag, MyUsername, HashTag};
+	MyUsername = lists:nth(1, MessageTokens),
+	HashTag = lists:nth(2, MessageTokens),
+	engine ! {followHashTag, MyUsername, HashTag};
 handleRequest(_State, "sendTweet", MessageTokens) ->
-		MyUsername = lists:nth(1, MessageTokens),
-		OriginalTweeter = lists:nth(2, MessageTokens),
-		Message = lists:nth(3, MessageTokens),
-		Tweet = #tweet{text = Message, hashTags = [], mentions = [], originalTweeter = OriginalTweeter, actualTweeter = MyUsername},
-		engine ! {sendTweet, MyUsername, Tweet};
+	MyUsername = lists:nth(1, MessageTokens),
+	OriginalTweeter = lists:nth(2, MessageTokens),
+	Message = lists:nth(3, MessageTokens),
+	io:fwrite("Before Hash\n"),
+	HashTags = extract("#", Message),
+	printList(HashTags),
+	io:fwrite("Before Mention\n"),
+	Mentions = extract("@", Message),
+	printList(Mentions),
+	io:fwrite("After all\n"),
+	Tweet = #tweet{text = Message, hashTags = HashTags, mentions = Mentions, originalTweeter = OriginalTweeter, actualTweeter = MyUsername},
+	engine ! {sendTweet, MyUsername, Tweet};
 handleRequest(_, _, _) ->
-			io:fwrite("Unknown message recieved\n").
+	io:fwrite("Unknown message recieved\n").
 
 formatTweetsForSending(CurrentMessage, []) ->
 	CurrentMessage;
 formatTweetsForSending(CurrentMessage, [CurrentTweet | RemainingTweets]) ->
-	io:fwrite("!!FormatBegin\n"),
 	AddToMessage = formatTweetForSending(CurrentTweet),
-	io:fwrite("!!FormatNext\n"),
 	formatTweetsForSending(CurrentMessage ++ AddToMessage, RemainingTweets).
 
 formatTweetForSending(Tweet) ->
-	Result = binary_to_list(Tweet#tweet.actualTweeter) ++ "|" ++ binary_to_list(Tweet#tweet.originalTweeter) ++ "|" ++ binary_to_list(Tweet#tweet.text) ++ "^",
+	Result = Tweet#tweet.actualTweeter ++ "|" ++ Tweet#tweet.originalTweeter ++ "|" ++ Tweet#tweet.text ++ "^",
 	Result.
 
 % Recieve messages from the engine
 websocket_info({recieveQuery, Data}, State) ->
-	io:fwrite("!!recieveQuery\n"),
 	SendTweets = list_to_binary(formatTweetsForSending("", Data)),
-	io:fwrite("!!FormatOver\n"),
 	{reply, {text, SendTweets}, State};
 websocket_info({publishTweet, Data}, State) ->
-	io:fwrite("!!publishTweet\n"),
 	io:write(Data),
 	SendTweet = list_to_binary(formatTweetsForSending("", [Data])),
-	io:fwrite("!!FormatOver\n"),
 	{reply, {text, SendTweet}, State};
 websocket_info(_Request, _State) ->
 	ok.
+
+extract(Cue, Str) ->
+    case re:run(Str,concat(Cue, "[^@#\t\s\n$]+"), [global, notempty]) of 
+        {match, Captured} -> 
+            [substr(Str, element(1, lists:nth(1, Group)) + 2, element(2, lists:nth(1, Group)) - 1) || Group <- Captured, true];
+        nomatch ->
+            []
+    end.
+
+binListToStringList(ResultList, []) ->
+	ResultList;
+binListToStringList(ResultList, [BinListEntry | BinListRemaining]) ->
+	binListToStringList(ResultList ++ [binary_to_list(BinListEntry)], BinListRemaining).
+
+printList(List) ->
+     io:fwrite("~w~n",[List]).
